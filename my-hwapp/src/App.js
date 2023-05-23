8e4fa840b633b099a1b29dd7e2adb6395d415fcd
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-05-23 19:49:23 -0400

    Complete HW

---
+++ my-hwapp/src/App.js
@@ -0,0 +1,12 @@
+import { GreenBox } from './GreenBox';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GreenBox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-hwapp/src/GreenBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './GreenBox.css'
+
+export const GreenBox = () => {
+  return (
+    <div className='GreenBox'>
+        Hello, World!
+    </div>
+  )
+}
